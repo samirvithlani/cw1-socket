c80264711da375785f35c1a738fe348a004e0a0d
samirvithlani <samir.vithlani83955@gmail.com>
2024-03-31 12:52:16 +0530

    node

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const http = require('http');
 const app = express();
+const cors = require('cors');
+
 const server = http.createServer(app);
 app.use(express.static('public'));
+const socketIO = require("socket.io");
 
-const {Server} = require('socket.io');
-const io = new Server(server);
+const io = socketIO(server, {
+    cors: {
+      origin: "http://localhost:3001", // Adjust with your React app's URL
+      methods: ["GET", "POST"],
+    },
+  });
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+  app.use(cors());
 
 io.on('connection',(socket)=>{
     console.log('A user connected'+socket.id);
